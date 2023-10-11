2b1d47bb50d5e291ea90e06c9ab1272e3b521851
Azamat966 <reallygoooool01@gmail.com>
2023-10-11 17:51:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,144 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+
+//        System.out.println("Четные");
+//        for (int i = 1; i < 22; i++) {
+//            if (i % 2 == 0) {
+//                System.out.print(i+" ");
+//
+//
+//            }
+//        }
+//        System.out.println();
+//        System.out.println("Нечетные");
+//                for(int i=0; i < 20; i++) {
+//                            if(i%2 == 0) {
+//                continue;
+//            }
+//            System.out.print(i+" ");
+//        }
+//    }
+//}
+
+
+//        for (int i = 10; i > 0; i--) {
+//            System.out.print(i+" ");
+//
+//
+//        }
+//        System.out.print(0);
+//        for (int i = 1; i <=20 ; i++) {
+//            System.out.print("-"+i);
+
+//
+//        for (int i = 10; i > 0; i--) {
+//            System.out.println(i);
+//
+//
+//        }
+//        System.out.println(0);
+//        for (int i = 1; i <=10 ; i++) {
+//            System.out.println("-"+i);
+//
+//        }
+//
+//
+//
+//
+//        }
+
+        //}}
+//        for (int i = 50; i <101 ; i++) {
+//            System.out.println(i);
+//
+//        }
+//
+//        }
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        int b = scanner.nextInt();
+//        int c = 0;
+//            while (b>c){
+//                for (int i = 0; i < scanner.nextInt()+ i++;)
+//            c++;
+//        }if  (b<c){
+//            System.out.println();
+
+
+        // Scanner scanner = new Scanner(System.in);
+//            {
+//                int N = scanner.nextInt();
+//                for (int i = 1; i <= 10; i++) {
+//                    System.out.println(N + " * " + i + " = " + N * i);
+//                }
+//            }
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        int a = 1;
+//        int number;
+//        while (true){
+//            System.out.println("веддите 0 для завершения");
+//            number= scanner.nextInt();
+//
+//
+//        if (number==0) {
+//       break;
+//
+//
+//
+//
+//        }
+//
+//
+//
+//
+//    }
+//        }
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = 99;
+//        while (a < b ) {
+//            a++;
+//            System.out.println(a);
+//        }
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        int b = scanner.nextInt();
+//        int c = 0;
+//            while (b>c){
+//                for (int i = 0; i < scanner.nextInt()+ i++;)
+//            c++;
+//        }if  (b<c){
+//            System.out.println("Е брат котуно еле кирчи");
+//
+//        }
+//        }
+
+
+//      Scanner scanner = new Scanner(System.in);
+//      int number = scanner.nextInt();
+//      int frist = 0;
+//      while (number > frist) {
+//        frist++;
+//        if (frist % 2 == 0) {
+//          System.out.println(frist);
+//        }
+//      }
+//
+////
+
+
+    }
